c9fb326fb4ddf3af6b841e99a573672aa255cea1
Prajwal K Manjunath <prajwalkman@gmail.com>
2013-05-19 11:52:04 +0530

    initial commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+// console.log(window.angular);
+
+app = angular.module('app', ['uiSlider']);
+
+app.controller('ItemCtrl', ['$scope', function($scope){
+  $scope.item = {
+    name: 'Potato',
+    cost: 350
+  };
+  $scope.currencyFormatting = function(value) { return value.toString() + " $"; };
+}]);
+
+app.controller('PositionCtrl', ['$scope', function($scope){
+  $scope.position = {
+    name: 'Potato Master',
+    minAge: 25,
+    maxAge: 40
+  };
+}]);
