75ff70485ee3de57818c744c212e3646f4046d62
metaprogrammed <nati.aranguren@gmail.com>
2017-01-18 15:36:03 -0500

    Add on move stop attr

--- slider.js
+++ slider.js
@@ -94,11 +94,12 @@
         highlight: '@',
         precision: '@',
         buffer: '@',
-        dragstop: '=',
+        dragstop: '@',
         block: '=',
         ngModel: '=?',
         ngModelLow: '=?',
-        ngModelHigh: '=?'
+        ngModelHigh: '=?',
+        onMoveStop: '='
       },
       template: '<div class="bar"><div class="selection"></div></div>\n<div class="handle low"></div><div class="handle high"></div>\n<div class="bubble limit low">{{ values.length ? values[floor || 0] : floor }}</div>\n<div class="bubble limit high">{{ values.length ? values[ceiling || values.length - 1] : ceiling }}</div>\n<div class="bubble value low">{{ values.length ? values[local.ngModelLow || local.ngModel || 0] : local.ngModelLow || local.ngModel || 0 }}</div>\n<div class="bubble value high">{{ values.length ? values[local.ngModelHigh] : local.ngModelHigh }}</div>',
       compile: function(element, attributes) {
@@ -227,6 +228,7 @@
                     scope[low] = scope.local[low];
                   }
                   currentRef = ref;
+                  scope.onMoveStop();
                   return scope.$apply();
                 };
                 onMove = function(event) {
